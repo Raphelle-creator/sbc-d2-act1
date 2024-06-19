cc342981c351fedddb5b7089e9c9018bb35036ba
Raphelle <raphelle.lansaderas@aclcbutuan.edu.ph>
2024-06-18 22:16:31 -0700

    sbc-d2-act1

---
+++ sbc-d2-act1.py
@@ -0,0 +1,11 @@
+word = input("Word: ")
+print(word.title())
+print(word.capitalize().replace("p", "P"))
+print(word[-8:-4].replace("b", "L"))      
+print(word[-4:].replace("p", "E"))
+print(word[12].upper() + word[5].upper())
+print(word.replace(" ", "ss").isalpha())
+
+
+
+
